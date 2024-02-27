3b8395babb7d49218d4690bb0c848af7fa9d3b62
shohan-cnits <shohan.cnits@gmail.com>
2024-02-27 13:02:17 +0600

    slider complted

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -39,3 +39,58 @@ productTabBtn.forEach((tab, index) => {
     productTabContent[index].classList.add("active");
   });
 });
+
+
+//second tab
+const productTabBoxs = document.querySelector(".product_menu_item_tabs"),
+  productArrowIcon = document.querySelectorAll(".product_tab_arrow_icons");
+
+  productArrowIcon.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    productTabBoxs.scrollTop += icon.id === "top" ? -200 : 200;
+  });
+});
+
+let isDraggingProducts = false;
+const productDraggings = (e) => {
+  if (!isDraggingProducts) return;
+  productTabBoxs.classList.add("productDraggings");
+  productTabBoxs.scrollTop -= e.movementX;
+};
+
+const productDragStops = () => {
+  productTabBoxs.classList.remove("productDraggings");
+  isDraggingProducts = false;
+};
+
+productTabBoxs.addEventListener("mousedown", () => (isDraggingProducts = true));
+productTabBoxs.addEventListener("mousemove", productDraggings);
+document.addEventListener("mouseup", productDragStops);
+
+
+//Thrid tab
+
+const productTabBoxa = document.querySelector(".product_menu_item_taba"),
+  productArrowIcona = document.querySelectorAll(".product_tab_arrow_icona");
+
+  productArrowIcona.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    productTabBoxa.scrollTop += icon.id === "top" ? -200 : 200;
+  });
+});
+
+let isDraggingProducta = false;
+const productDragginga = (e) => {
+  if (!isDraggingProducta) return;
+  productTabBoxa.classList.add("productDragginga");
+  productTabBoxa.scrollTop -= e.movementX;
+};
+
+const productDragStopa = () => {
+  productTabBoxa.classList.remove("productDragginga");
+  isDraggingProducta = false;
+};
+
+productTabBoxa.addEventListener("mousedown", () => (isDraggingProducta = true));
+productTabBoxa.addEventListener("mousemove", productDragginga);
+document.addEventListener("mouseup", productDragStopa);
