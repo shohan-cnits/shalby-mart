e24e8a161ef11e9bfc1283c4d4bb80ff4829d3ee
unknown <shohan.cnits@gmail.com>
2024-03-14 13:08:28 -0700

    select box add topbar

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -6,11 +6,26 @@ selectBtn.addEventListener('click', function() {
     selectBtn.classList.toggle('active');
 });
 
-console.log(".add");
-
 for (options of option) {
     options.onclick = function() {
         text.innerHTML = this.textContent;
         selectBtn.classList.remove('active');
+    }
+};
+
+
+//2nd
+const selectBtns = document.getElementById('topbar_btns');
+const texts = document.getElementById('topbar_texts');
+const optiona = document.getElementsByClassName('topbar_options');
+
+selectBtns.addEventListener('click', function() {
+    selectBtns.classList.toggle('active');
+});
+
+for (options of optiona) {
+    options.onclick = function() {
+        texts.innerHTML = this.textContent;
+        selectBtns.classList.remove('active');
     }
 };
